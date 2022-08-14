5b32bb4732ed69ca5b1613005c602f830275b458
эдиль <edu007super@gmail.com>
2022-08-14 18:10:42 +0600

    added adaptive menu

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@
 		}
 	};
 }());
+
+//burger headler
+
+(function () {
+	const burgerItem = document.querySelector('.burger');
+	const menu = document.querySelector('.header_nav');
+	const menuCloseItem = document.querySelector('.header__nav-close');
+	burgerItem.addEventListener('click', () => {
+		menu.classList.add('header_nav_active');
+	});
+	menuCloseItem.addEventListener('click', () =>{
+		menu.classList.remove('header_nav_active');
+	});
+}());
